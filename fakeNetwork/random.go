aba19d482e256b9bcd05754aacb7f489f7ebc695
tigerbot <seth.gibelyou@daplie.com>
2018-06-25 18:22:42 -0600

    added a new network emulator for testing purposes

---
+++ fakeNetwork/random.go
@@ -0,0 +1,22 @@
+package fakeNetwork
+
+import (
+	"math/rand"
+	"net"
+)
+
+// RandomAddress creates a new *net.UDPAddr with a random IP address and port. The argument
+// ipv6 indicates whether IPv6 should be included, and as such this can return IPv4 addresses.
+func RandomAddress(ipv6 bool) net.Addr {
+	var addr net.UDPAddr
+	if ipv6 && rand.Intn(3) > 0 {
+		addr.IP = make(net.IP, 16)
+	} else {
+		addr.IP = make(net.IP, 4)
+	}
+	// According to https://golang.org/pkg/math/rand/#Read this always returns a nil error.
+	rand.Read(addr.IP)
+	addr.Port = 1024 + rand.Intn(1<<16-1024)
+
+	return &addr
+}
---
+++ fakeNetwork/random_test.go
@@ -0,0 +1,49 @@
+package fakeNetwork_test
+
+import (
+	"math/rand"
+	"net"
+	"testing"
+	"time"
+
+	. "gitlab.daplie.com/core-sdk/hyperdht/fakeNetwork"
+)
+
+func TestRandAddrIPv4(t *testing.T) {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+	t.Logf("seeded random with %d", seed)
+
+	for i := 0; i < 200 && !t.Failed(); i++ {
+		addr := RandomAddress(false).(*net.UDPAddr)
+		if addr.IP.To4() == nil {
+			t.Errorf("invalid IPv4 address produced: %s", addr.IP)
+		}
+		if addr.Port < 1024 || addr.Port >= 1<<16 {
+			t.Errorf("invalid port produced: %d", addr.Port)
+		}
+	}
+}
+
+func TestRandAddrIPv6(t *testing.T) {
+	seed := time.Now().Unix()
+	rand.Seed(seed)
+	t.Logf("seeded random with %d", seed)
+
+	var ipv4 int
+	for i := 0; i < 200 && !t.Failed(); i++ {
+		addr := RandomAddress(true).(*net.UDPAddr)
+		if addr.IP.To16() == nil {
+			t.Errorf("invalid IPv6 address produced: %s", addr.IP)
+		} else if addr.IP.To4() != nil {
+			ipv4++
+		}
+		if addr.Port < 1024 || addr.Port >= 1<<16 {
+			t.Errorf("invalid port produced: %d", addr.Port)
+		}
+	}
+
+	if ipv4 < 40 {
+		t.Errorf("IPv6 address creation made %d IPv4 addresses, expected at least 40", ipv4)
+	}
+}
